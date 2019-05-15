c853e16d6502ed0cb6cdc81259ee642c3e9765e8
UnusualPeter <50661041+UnusualPeter@users.noreply.github.com>
2019-05-15 12:48:50 +0200

    primer commit

---
+++ precache-manifest.f0b3c731d7d7712e874ca0596031c33c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8473ab7fa27f756ad96e33c089c15344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82f1e93daf29afe51885",
+    "url": "/static/css/main.29a205ca.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "82f1e93daf29afe51885",
+    "url": "/static/js/main.49acc91e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
